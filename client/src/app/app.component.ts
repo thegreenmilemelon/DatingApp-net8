26f9d0e260191a5bc4a9e5465138e0a50ae28e35
thegreenmilemelon <suren78621@gmail.com>
2024-09-15 13:19:58 +0545

    end of section 5: client registration

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -5,11 +5,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
 import { AccountService } from './_services/account.service';
 import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgFor, NavComponent, FormsModule],
+  imports: [RouterOutlet, NgFor, NavComponent, FormsModule, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
